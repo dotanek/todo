bf24d010118e4e7f0f8a3e19a18b67889ac7cede
unknown <dotanek222@gmail.com>
2020-09-28 13:05:25 +0200

    Completed task styling.

--- src/components/List/Task.jsx
+++ src/components/List/Task.jsx
@@ -1,22 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-    position: relative;
-    display: flex;
-    width: 100%;
-    height: 30px;
-    border: 1px solid rgba(0,0,0,0.1);
-    border-radius: 5px;
-    margin-top: 4px;
-    color: #303030;
-    background-color: ${props => props.taskActive ? 'rgba(0,0,255,0.1)' : 'none'};
-    
-    &:hover {
-        background-color: rgba(0,0,0,0.1);
-        cursor: pointer;
-    } 
-`
+import styled, { css } from 'styled-components';
 
 const Checkbox = styled.input`
     position: absolute;
@@ -37,9 +20,11 @@ const Checkbox = styled.input`
         margin: 8px;
     }
 
-    &:checked {
-        background-color: #404040;
-    }
+    ${props => props.completed && css`
+        background-color: #999999;
+        border: 1px solid #999999;
+    `};
+
 `
 
 const Text = styled.div`
@@ -52,6 +37,29 @@ const Text = styled.div`
     font-size: 14px;
     white-space: nowrap;
     overflow: hidden;
+
+    ${props => props.completed && css`
+        text-decoration-line: line-through;
+        text-decoration-color: #999999;
+        color: #999999;
+    `};
+`
+
+const Container = styled.div`
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 30px;
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 5px;
+    margin-top: 4px;
+    color: #303030;
+    background-color: ${props => props.taskActive ? 'rgba(0,0,255,0.1)' : 'none'};
+    
+    &:hover {
+        background-color: rgba(0,0,0,0.1);
+        cursor: pointer;
+    } 
 `
 
 class Task extends Component {
@@ -64,9 +72,9 @@ class Task extends Component {
 
     render() { 
         return (
-            <Container onClick={this.props.onClickTask} taskActive={this.props.taskActive}>
-                <Checkbox type='checkbox' onClick={this.onClickCheckbox}/>
-                <Text>{this.props.task.title}</Text>
+            <Container completed={this.props.task.completed} onClick={this.props.onClickTask} taskActive={this.props.taskActive}>
+                <Checkbox completed={this.props.task.completed} type='checkbox' onClick={this.onClickCheckbox}/>
+                <Text completed={this.props.task.completed}>{this.props.task.title}</Text>
             </Container>
         );
     }
